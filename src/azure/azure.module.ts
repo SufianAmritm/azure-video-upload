f3099b8a3d0b6809b2882a1ba87c29528f5774a2
Zeeshan <m.zeeshan.ashraf.633@gmail.com>
2024-07-06 04:59:25 +0500

    custom multer storage

--- src/azure/azure.module.ts
+++ src/azure/azure.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AzureService } from './azure.service';
 import { AzureController } from './azure.controller';
+import { MulterAzureStorage } from 'multer-azure-blob-storage';
+import { MulterModule } from '@nestjs/platform-express';
+import { getStorage } from 'src/multer/storage';
 
 @Module({
+  imports: [],
   providers: [AzureService],
-  controllers: [AzureController]
+  controllers: [AzureController],
 })
 export class AzureModule {}
